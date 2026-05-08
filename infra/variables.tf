54c88795a65cbeb6a3625646d60df02b8ba8faea
lhdenis <lhdenis@bordeaux-inp.fr>
2026-05-08 16:50:26 +0200

    init: structure de base VPC + EKS

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "Région AWS"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "cluster_name" {
+  description = "cluster name"
+  type        = string  
+  default = "cluster"
+}
