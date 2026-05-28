3575c57158058331ea587f57e7319a909effe67f
lhdenis <louishadrien35000@gmail.com>
2026-05-29 00:46:34 +0200

    aws_ecs_service

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -62,7 +62,7 @@ resource "aws_ecs_service" "main" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.task_definition.arn
   desired_count   = 3
-  depends_on = [aws_lb_listener.http]
+  depends_on = [aws_lb_listener.lb-listener]
 
    network_configuration {
     subnets          = var.private_subnet_ids
